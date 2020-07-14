c6245bfbf17f37b635f4680384f6ea7034353908
Nick Mudge <nick@perfectabstractions.com>
2020-07-14 10:04:50 -0400

    dev

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ let { div, body, p } = builders(createElements)
 
 const app =
   div(
-    p`cool man!!`
+    p`cool man!! now???`,
+    div
+      .id`g_id_onload`
+      .data·client_id`615157284989-q0rmkt4g9rfdinui6a6k5mua02tbknvk.apps.googleusercontent.com`
+      .data·login_uri`https://personal.diamonds`
+      .data·your_own_param_1_to_login`any_value`
+      .data·your_own_param_2_to_login`any_value`
   )
 
 document.body = body(app)
